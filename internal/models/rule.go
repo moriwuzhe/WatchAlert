a2123908f8c0e7dea9f8b6e36694f7e3bf46e5c3
jp <x@s.com>
2025-04-10 00:41:29 +0800

    fat

--- internal/models/rule.go
+++ internal/models/rule.go
@@ -5,7 +5,7 @@ type NoticeRoutes []map[string]string
 type AlertRule struct {
 	//gorm.Model
 	TenantId             string        `json:"tenantId"`
-	RuleId               string        `json:"ruleId" gorm:"ruleId"`
+	RuleId               string        `json:"ruleId" gorm:"rule_id;type:VARCHAR(255)"`
 	RuleGroupId          string        `json:"ruleGroupId"`
 	DatasourceType       string        `json:"datasourceType"`
 	DatasourceIdList     []string      `json:"datasourceId" gorm:"datasourceId;serializer:json"`
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,16 +1,16 @@
 package models
 
 type Member struct {
-	UserId     string    `json:"userid"`
-	UserName   string    `json:"username"`
-	Email      string    `json:"email"`
-	Phone      string    `json:"phone"`
-	Password   string    `json:"password"`
-	Role       string    `json:"role"`
-	CreateBy   string    `json:"create_by"`
-	CreateAt   int64     `json:"create_at"`
-	JoinDuty   string    `json:"joinDuty" `
-	DutyUserId string    `json:"dutyUserId"`
+	UserId     string   `json:"userid" gorm:"user_id;type:VARCHAR(255)"`
+	UserName   string   `json:"username" gorm:"user_name;type:VARCHAR(255)"`
+	Email      string   `json:"email"`
+	Phone      string   `json:"phone"`
+	Password   string   `json:"password"`
+	Role       string   `json:"role"`
+	CreateBy   string   `json:"create_by"`
+	CreateAt   int64    `json:"create_at"`
+	JoinDuty   string   `json:"joinDuty" gorm:"join_duty;type:VARCHAR(255)"`
+	DutyUserId string   `json:"dutyUserId" gorm:"duty_user_id;type:VARCHAR(255)"`
 	Tenants    []string `json:"tenants" gorm:"tenants;serializer:json"`
 }
 
